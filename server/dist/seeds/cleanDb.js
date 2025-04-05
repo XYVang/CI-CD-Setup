38cab6c0485c98e13975bd68be3e7da70e4cbc4d
Xavier <xaviervang1999@gmail.com>
2025-04-05 05:50:01 -0500

    npm files

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,20 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        const model = models[modelName]; // Store the model reference
+        // Check if the model or model.db is undefined to avoid accessing properties on undefined
+        if (!model || !model.db || !model.db.db) {
+            throw new Error(`Model "${modelName}" or its database connection not found`);
+        }
+        let modelExists = await model.db.db.listCollections({
+            name: collectionName
+        }).toArray();
+        if (modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+};
