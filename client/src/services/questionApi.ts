a0e59bb3a492070b9770efd7efde3897a3489839
Xavier <xaviervang1999@gmail.com>
2025-04-05 07:59:28 -0500

    Middleware to connect both ends on Render

--- client/src/services/questionApi.ts
+++ client/src/services/questionApi.ts
@@ -1,8 +1,11 @@
 import type { Question } from '../models/Question.js';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const getQuestions = async (): Promise<Question[]> => {
   try {
-    const response = await fetch('/api/questions/random');
+    // Use the apiUrl for the request
+    const response = await fetch(`${apiUrl}/api/questions/random`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
